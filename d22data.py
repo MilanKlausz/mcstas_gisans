64ac423dcd064a3fdb0e10cfa81ef4d008d97aa5
MilanKlausz <Milan.Klausz@ess.eu>
2024-05-22 09:55:32 +0200

    read data measured at d22

---
+++ d22data.py
@@ -0,0 +1,43 @@
+"""
+Read data from measurements at D22(ILL) from nxs files
+"""
+
+import h5py
+import numpy as np
+from numpy import pi, sin, arctan
+
+def getStoredData(filepath='073174.nxs'):
+  # Constants
+  lambda_angstrom = 6  # Wavelength in angstroms
+  detector_distance_mm = 17600  # Distance from sample to detector in mm
+  pixel_size_x_mm = 4  # Pixel size in x-direction in mm
+  pixel_size_y_mm = 8  # Pixel size in y-direction in mm
+
+  # Open the NeXus file
+  with h5py.File(filepath, 'r') as file:
+    detector_data = file['entry0/D22/Detector 1/data1'][:]
+  hist = detector_data[:,:,0]
+  histError = np.sqrt(hist)
+
+  # Compute q-values for x and y axes
+  x_pixels = np.arange(0, detector_data.shape[1] + 1) # Start from 0
+  y_pixels = np.arange(0, detector_data.shape[0] + 1) # Start from 0
+
+  # Convert pixel index to mm
+  x_mm = x_pixels * pixel_size_x_mm
+  y_mm = y_pixels * pixel_size_y_mm
+
+  # Apply offset based of values to match the specular peak
+  x_mm = x_mm - x_mm.max()/2 - (pixel_size_x_mm * 3) #TODO hardcoded
+  y_mm = y_mm - y_mm.max()/2 + (pixel_size_y_mm * 37) #TODO hardcoded
+
+  # Convert mm to q
+  theta_x = arctan(x_mm / detector_distance_mm)/2
+  theta_y = arctan(y_mm / detector_distance_mm)/2
+  q_x = (4 * pi / lambda_angstrom) * sin(theta_x)
+  q_y = (4 * pi / lambda_angstrom) * sin(theta_y)
+
+  q_x *= 10 #from 1/A to 1/nm
+  q_y *= 10 #from 1/A to 1/nm
+
+  return hist, histError, q_x, q_y
